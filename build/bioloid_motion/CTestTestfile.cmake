2606c93f83ac619e134f5edaf0a19b023a9e0474
Teo Cardoso <teocardoso1@gmail.com>
2019-02-01 16:07:16 -0300

    build and devel folder removed from git.

--- build/bioloid_motion/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/teo/Documentos/nelso/src/bioloid_motion
-# Build directory: /home/teo/Documentos/nelso/build/bioloid_motion
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
